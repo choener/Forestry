b6904313e5198b66f5e49d3c92a913269f91aebc
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2017-11-23 18:50:23 +0100

    two run-scripts for benchmarking

---
+++ examples/per.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+RNAfold="/scratch/choener/Software/ViennaRNA-2.3.5/bin/RNAfold"
+
+# which sequence lengths to use
+for i in `seq 25 25 200`
+do
+  printf "%4d " $i
+  zcat examples/0300.input.gz | cut -c-$i | \
+    $RNAfold -p | \
+    time -f "%U" "$@" \
+    > /dev/null \
+    2>tmp.tmp
+    SEC=`cat tmp.tmp`
+    printf "%0.3f\n" $(bc -l <<< "$SEC / 50")
+done
